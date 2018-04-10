1235736fcaf28d8b6539ccfd4df24082daeac502
gyorodidanielg <gyorodidani5@freemail.hu>
2018-04-10 13:15:38 +0200

    docker_telepito_v3

--- docker-install.sh
+++ docker-install.sh
@@ -1,7 +1,14 @@
+sudo apt update
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-apt-cache policy docker-ce
-sudo apt-get install -y docker-ce
-sudo systemctl status docker
-sudo usermod -aG docker ${ubuntu}
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt update
+sudo apt install -y docker-ce
